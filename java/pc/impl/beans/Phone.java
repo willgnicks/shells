811bf516d1cadeebe88750b1118488f8a4426feb
willgnicks <gnicksfaith@gmail.com>
2022-08-08 00:48:39 +0800

    changes

--- java/pc/impl/beans/Phone.java
+++
@@ -1,7 +0,0 @@
-package com.sec.pc.impl.beans;
-
-public class Phone extends Product {
-    public Phone(int serialID, String type, String name, int price) {
-        super(serialID, type, name, price,0);
-    }
-}
